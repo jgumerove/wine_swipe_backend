856b06dd5c0afa57b25fde7e822f9cca40c4ddff
jgumerove <josh.gumerove@gmail.com>
2021-09-08 21:43:56 -0400

    add seed data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Wine.create(name: "2019 Sonama Coast", category: "Pinot Noir", image: "https://www.duckhornwineshop.com/assets/images/products/pictures/2018-decoy-california-pinot-noir.jpg")
+Wine.create(name: "Dom Perignon", category: "Champagne", image: "https://cdn.enviedechamp.com/4833-large_default/vintage-2008-magnum.jpg")
+Wine.create(name: "Hectare", category: "Merlot", image: "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/had/h53/12274214830110.png")
