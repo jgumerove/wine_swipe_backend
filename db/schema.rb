5d5d665fb8d52aa6db7c2a25ac38a953ff41db35
jgumerove <josh.gumerove@gmail.com>
2021-09-15 17:32:59 -0400

    create migration to add User to Wine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_010117) do
+ActiveRecord::Schema.define(version: 2021_09_15_212928) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "wines", force: :cascade do |t|
     t.string "name"
