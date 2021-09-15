db6a9ccbd3f238dff3107c10af1d010e74265ec7
jgumerove <josh.gumerove@gmail.com>
2021-09-15 17:58:56 -0400

    add username attribute to wine serializer

--- app/serializers/wine_serializer.rb
+++ app/serializers/wine_serializer.rb
@@ -1,3 +1,7 @@
 class WineSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :category, :username, :user_id
+  attributes :id, :name, :image, :category, :username
+
+    def username
+      self.object.user.username
+    end
 end
