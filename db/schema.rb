60adcfd338cb5a54f7776f8059d1433478b7d164
jgumerove <josh.gumerove@gmail.com>
2021-09-15 17:39:48 -0400

    add belongs_to has_many relationship for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_212928) do
+ActiveRecord::Schema.define(version: 2021_09_15_213138) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2021_09_15_212928) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_wines_on_user_id"
   end
 
+  add_foreign_key "wines", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Wine.create(name: "2019 Sonama Coast", category: "Pinot Noir", image: "https://www.duckhornwineshop.com/assets/images/products/pictures/2018-decoy-california-pinot-noir.jpg")
-Wine.create(name: "Dom Perignon", category: "Champagne", image: "https://cdn.enviedechamp.com/4833-large_default/vintage-2008-magnum.jpg")
-Wine.create(name: "Hectare", category: "Merlot", image: "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/had/h53/12274214830110.png")
+user1 = User.create(username: "josh.gumerove")
+user2 = User.create(username: "gumerove")
+user3 = User.create(username: "jgumerove")
+
+user1.wines.create(name: "2019 Sonama Coast", category: "Pinot Noir", image: "https://www.duckhornwineshop.com/assets/images/products/pictures/2018-decoy-california-pinot-noir.jpg")
+user2.wines.create(name: "Dom Perignon", category: "Champagne", image: "https://cdn.enviedechamp.com/4833-large_default/vintage-2008-magnum.jpg")
+user3.wines.create(name: "Hectare", category: "Merlot", image: "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/had/h53/12274214830110.png")
